d7f90ac86acc493d12bbad042b57ce5520af67d7
Toshiaki Maki <tmaki@vmware.com>
2021-08-03 12:50:13 +0900

    Use latest Reactor Core API

--- src/main/java/com/example/demokafkaconsumer/DemoKafkaConsumerApplication.java
+++ src/main/java/com/example/demokafkaconsumer/DemoKafkaConsumerApplication.java
@@ -1,34 +1,37 @@
 package com.example.demokafkaconsumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.UnicastProcessor;
 
 @SpringBootApplication
 @RestController
 public class DemoKafkaConsumerApplication {
 
-    private final UnicastProcessor<String> hotSource = UnicastProcessor.create();
-    private final Flux<String> messages = hotSource.publish()
-            .autoConnect().share();
+	private final Sinks.Many<String> sink = Sinks.many().replay().limit(1000);
 
+	private final Logger log = LoggerFactory.getLogger(DemoKafkaConsumerApplication.class);
 
-    @GetMapping(path = "/messages", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> messages() {
-        return this.messages.log("messages");
-    }
+	@GetMapping(path = "/messages", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> messages() {
+		return this.sink.asFlux();
+	}
 
-    @KafkaListener(topics = "${sample.topic}")
-    public void onMessage(String message) {
-        this.hotSource.onNext(message);
-    }
+	@KafkaListener(topics = "${sample.topic}")
+	public void onMessage(String message) {
+		log.info("onMessage({})", message);
+		this.sink.tryEmitNext(message);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoKafkaConsumerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKafkaConsumerApplication.class, args);
+	}
 }

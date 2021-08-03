048033dedcfe3e0c6a4ec59baeb75b0f15fab34f
Toshiaki Maki <tmaki@vmware.com>
2021-08-03 16:09:11 +0900

    Reactive version

--- src/main/java/com/example/demokafkaconsumer/DemoKafkaConsumerApplication.java
+++ src/main/java/com/example/demokafkaconsumer/DemoKafkaConsumerApplication.java
@@ -1,34 +1,40 @@
 package com.example.demokafkaconsumer;
 
+import java.util.List;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Sinks;
+import reactor.kafka.receiver.ReceiverOptions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.http.MediaType;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoKafkaConsumerApplication {
+	private final ReactiveKafkaConsumerTemplate<Object, String> kafkaTemplate;
 
-	private final Sinks.Many<String> sink = Sinks.many().replay().limit(1000);
+	public DemoKafkaConsumerApplication(@Value("${sample.topic}") String topic, KafkaProperties properties) {
+		this.kafkaTemplate = new ReactiveKafkaConsumerTemplate<>(ReceiverOptions.<Object, String>create(properties.buildConsumerProperties())
+				.subscription(List.of(topic)));
+	}
 
 	private final Logger log = LoggerFactory.getLogger(DemoKafkaConsumerApplication.class);
 
 	@GetMapping(path = "/messages", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<String> messages() {
-		return this.sink.asFlux();
-	}
-
-	@KafkaListener(topics = "${sample.topic}")
-	public void onMessage(String message) {
-		log.info("onMessage({})", message);
-		this.sink.tryEmitNext(message);
+		return this.kafkaTemplate
+				.receiveAutoAck()
+				.doOnRequest(n -> log.info("request({})", n))
+				.map(ConsumerRecord::value);
 	}
 
 	public static void main(String[] args) {

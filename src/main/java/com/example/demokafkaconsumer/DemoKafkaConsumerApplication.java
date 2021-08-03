949eea36f8525a687e8b5565767e8aa87d35acde
Toshiaki Maki <tmaki@vmware.com>
2021-08-03 18:29:55 +0900

    Change autoStartup to false

--- src/main/java/com/example/demokafkaconsumer/DemoKafkaConsumerApplication.java
+++ src/main/java/com/example/demokafkaconsumer/DemoKafkaConsumerApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.function.Predicate.not;
 
 @SpringBootApplication
 @RestController
@@ -20,12 +24,22 @@ public class DemoKafkaConsumerApplication {
 
 	private final Logger log = LoggerFactory.getLogger(DemoKafkaConsumerApplication.class);
 
+	private final KafkaListenerEndpointRegistry endpointRegistry;
+
+	public DemoKafkaConsumerApplication(KafkaListenerEndpointRegistry endpointRegistry) {
+		this.endpointRegistry = endpointRegistry;
+	}
+
 	@GetMapping(path = "/messages", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<String> messages() {
+		this.endpointRegistry.getListenerContainers()
+				.stream()
+				.filter(not(MessageListenerContainer::isRunning))
+				.forEach(MessageListenerContainer::start);
 		return this.sink.asFlux();
 	}
 
-	@KafkaListener(topics = "${sample.topic}")
+	@KafkaListener(topics = "${sample.topic}", autoStartup = "false")
 	public void onMessage(String message) {
 		log.info("onMessage({})", message);
 		this.sink.tryEmitNext(message);
